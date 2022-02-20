f9c264550eac6e7a8b8bc8e7d87ab92fa9936ef3
diego carvalho <diegohsc@gmail.com>
2022-02-20 10:18:42 -0300

    correção da user

--- app/Models/user.ts
+++ app/Models/user.ts
@@ -3,7 +3,7 @@ import Hash from '@ioc:Adonis/Core/Hash'
 import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
 import Message from './Message'
 
-export default class user extends BaseModel {
+export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
@@ -23,7 +23,7 @@ export default class user extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: user) {
+  public static async hashPassword (user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
